62f8228625e6e5b13ba5cbc00fbc3306cc544de8
felipe ricardi <felipericardi5@gmail.com>
2023-10-01 20:52:07 -0300

    chore: adding support and connection to sqsite

--- main.go
+++ main.go
@@ -1,16 +1,40 @@
 package main
 
 import (
+	"database/sql"
 	albumController "example/gin-vinyl-store/controllers"
+	"fmt"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
 
 	"github.com/gin-gonic/gin"
 )
 
+var db *sql.DB
+
 func main() {
+	connectToDatabase()
+
 	router := gin.Default()
 	router.GET("/albums", albumController.GetAlbums)
 	router.GET("/albums/:id", albumController.GetAlbumByID)
 	router.POST("/albums", albumController.PostAlbums)
 
 	router.Run("localhost:8080")
+}
+
+func connectToDatabase() {
+	var err error
+	db, err = sql.Open("sqlite3", "database.sqlite")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if pingErr := db.Ping(); pingErr != nil {
+		log.Fatal(pingErr)
+	}
+
+	fmt.Println("Connected!")
 }
