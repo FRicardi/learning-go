08565b9296d7b47342e7ced28acedc12cc92c4c8
felipe ricardi <felipericardi5@gmail.com>
2023-10-01 22:55:52 -0300

    feat: using database on album service

--- services/albumService.go
+++ services/albumService.go
@@ -1,32 +1,67 @@
 package albumService
 
 import (
+	"database/sql"
 	"errors"
+	sqlite "example/gin-vinyl-store/database"
 	types "example/gin-vinyl-store/types"
 )
 
-var albums = []types.Album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan", Artist: "Sarah Vaughan", Price: 39.99},
+func GetAlbums() ([]types.Album, error) {
+	rows, err := sqlite.GetDB().Query("SELECT * FROM albums")
+
+	if err != nil {
+		return nil, errors.New("Could not find albums")
+	}
+
+	var albums []types.Album
+	defer rows.Close()
+
+	for rows.Next() {
+		var album types.Album
+
+		if err := rows.Scan(&album.ID, &album.Title, &album.Artist, &album.Price); err != nil {
+			return nil, errors.New("Albums not properly formatted")
+		}
+
+		albums = append(albums, album)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, errors.New("Error reading albums")
+	}
+
+	return albums, nil
 }
 
-func GetAlbums() []types.Album {
-	return albums
-}
+func AddAlbum(newAlbum types.Album) (int64, error) {
+	result, err := sqlite.GetDB().Exec("INSERT INTO album (title, artist, price) values (?, ?, ?)", newAlbum.Title, newAlbum.Artist, newAlbum.Price)
 
-func AddAlbum(newAlbum types.Album) types.Album {
-	albums = append(albums, newAlbum)
-	return newAlbum
+	if err != nil {
+		return 0, errors.New("Error creating album")
+	}
+
+	id, err := result.LastInsertId()
+
+	if err != nil {
+		return 0, errors.New("Error fetching album inserted")
+	}
+
+	return id, nil
 }
 
 func GetAlbumByID(id string) (*types.Album, error) {
+	row := sqlite.GetDB().QueryRow("SELECT * FROM albums WHERE id = ?", id)
 
-	for _, album := range albums {
-		if album.ID == id {
-			return &album, nil
+	var album types.Album
+
+	if err := row.Scan(&album.ID, &album.Title, &album.Artist, &album.Price); err != nil {
+		if err == sql.ErrNoRows {
+			return nil, errors.New("Album not found")
 		}
+
+		return nil, errors.New("Album not properly formatted")
 	}
 
-	return &types.Album{}, errors.New("Album not found")
+	return &album, nil
 }
