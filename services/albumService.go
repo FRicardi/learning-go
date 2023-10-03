0f2e50e72e3f3f9833f5864c4cdbe0b50f18443b
felipe ricardi <felipericardi5@gmail.com>
2023-10-02 23:23:31 -0300

    chore: remove prints

--- services/albumService.go
+++ services/albumService.go
@@ -4,16 +4,12 @@ import (
 	"errors"
 	connector "example/gin-vinyl-store/database"
 	types "example/gin-vinyl-store/types"
-	"fmt"
 )
 
 func GetAlbums() ([]types.Album, error) {
 	var albums []types.Album
 
 	result := connector.GetDB().Find(&albums)
-
-	fmt.Println(albums)
-	fmt.Println(result)
 
 	if result.Error != nil {
 		return nil, errors.New("Could not find albums")
@@ -36,8 +32,6 @@ func GetAlbumByID(id int64) (*types.Album, error) {
 	var album types.Album
 	result := connector.GetDB().First(&album, id)
 
-	fmt.Println(album)
-	fmt.Println(result)
 	if result.Error != nil {
 		return nil, errors.New("Album not found")
 	}
